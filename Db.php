8be5843c19dd3818b02193558bfeeeb336cd7316
benjbugandwa <127123043+benjbugandwa@users.noreply.github.com>
2023-07-25 16:07:02 +0200

    Update Db.php

--- Db.php
+++ Db.php
@@ -5,12 +5,12 @@
 function getconnexion()
 {
     $database='';
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
+    $servername = 'us-cdbr-east-06.cleardb.net';
+    $username = 'bdb618a206eb0e';
+    $password = 'eecc22ba';
 
     try{
-        $database=new PDO("mysql:host=$servername;dbname=myalertbookdb", $username, $password);
+        $database=new PDO("mysql:host=$servername;dbname=heroku_6c3fffb0324e409", $username, $password);
         $database->exec("SET NAMES 'utf8';");
         return $database;
     }
